24576eec0370dbfa8e8ea1c21980bbdff83122d2
Y1a2s3h4 <yashpurohit763@gmail.com>
2021-09-05 16:21:19 +0530

    Installed & configured tailwindcss. Added few elements & stylings.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <h1 className="text-center text-white font-medium text-2xl sm:text-4xl md:text-5xl font-display">
+        Chat App
+      </h1>
     </div>
   );
 }
